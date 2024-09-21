397784110ca6d71d10e21cd53e7d56ea7b21c3ef
Dbaraldi <Rubensjunior8216@gmail.com>
2024-09-20 21:57:54 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿﻿Console.Clear();
+ 
+Console.Write("0"); Thread.Sleep(1200);
+
+Console.Write("1");Thread.Sleep(1200);
+
+Console.Write("2");Thread.Sleep(1200);
+
+Console.Write("3");Thread.Sleep(1200);
+
+Console.Write("4");Thread.Sleep(1200);
+
+Console.Write("5");Thread.Sleep(1200);
+
+Console.Write("6");Thread.Sleep(1200);
+
+Console.Write("7");Thread.Sleep(1200);
+
+Console.Write("8");Thread.Sleep(1200);
+
+Console.Write("9");Thread.Sleep(1200);
+
+Console.Write("10.");
+ 
